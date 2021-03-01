7ad39ce64dec73abdb8a9aa984a615f9228b1e68
Alex Vear <av@axvr.io>
2021-03-01 23:48:22 +0000

    Greatly simplify Turing Machine simulator logic

--- 2021/turing/README.md
+++ 2021/turing/README.md
@@ -6,36 +6,24 @@ _28th February 2021_
 (require 'turing-machine)
 
 (def binary-increment-rules
-  {:a {nil {:write nil
-            :state :b
-            :move  1}
-       0 {:write 0
-          :state :a
-          :move  -1}
-       1 {:write 1
-          :state :a
-          :move  -1}}
-   :b {nil {:write 1
-            :state :c
-            :move  -1}
-       0 {:write 1
-          :state :c
-          :move  1}
-       1 {:write 0
-          :state :b
-          :move  1}}
-   :c {nil {:write nil
-            :state :halt
-            :move  1}
-       0 {:write 0
-          :state :c
-          :move  -1}
-       1 {:write 1
-          :state :c
-          :move  -1}}
-   :halt nil})
+  {:a {nil {:write nil, :state :b,    :move   1}
+       0   {:write 0,   :state :a,    :move  -1}
+       1   {:write 1,   :state :a,    :move  -1}}
+   :b {nil {:write 1,   :state :c,    :move  -1}
+       0   {:write 1,   :state :c,    :move   1}
+       1   {:write 0,   :state :b,    :move   1}}
+   :c {nil {:write nil, :state :halt, :move   1}
+       0   {:write 0,   :state :c,    :move  -1}
+       1   {:write 1,   :state :c,    :move  -1}}})
 
-(turing-machine/simulate [1 1 0 1] :a binary-increment-rules 0 nil)
+(:tape
+  (turing-machine/run
+    {:rules binary-increment-rules
+     :state :a
+     :tape [1 1 0 1]
+     :head 0
+     :blank nil}))
+;;; -> [nil 0 0 1 1]
 ```
 
 _Public domain.  No rights reserved._
--- 2021/turing/turing_machine.clj
+++ 2021/turing/turing_machine.clj
@@ -4,83 +4,68 @@
 ;;;; Public domain.  No rights reserved.
 
 
-;;; TODO: clean up this code.
-
-
 (ns turing-machine)
 
 
-(defn- lookup-rule [rules state value]
-  (get (rules state) value))
+(defn- expand-tape [{:keys [head tape blank] :as ar}]
+  (conj
+    ar
+    (cond
+      (neg? head) (let [diff (- 0 head)]
+                    {:tape (vec (concat (repeat diff blank) tape))
+                     :head 0})
+      (>= head (count tape)) (let [diff (- (inc head) (count tape))]
+                               {:tape (apply conj tape (repeat diff blank))
+                                :head head}))))
 
 
-(defn- expand-tape
-  "Simulate an infinite tape."
-  [next-index tape pad-with]
-  (cond
-    (neg? next-index) (let [diff (- 0 next-index)]
-                        {:new-tape  (vec (concat (repeat diff pad-with) tape))
-                         :new-index 0})
-    (>= next-index (count tape)) (let [diff (- (inc next-index) (count tape))]
-                                   {:new-tape  (apply conj tape (repeat diff pad-with))
-                                    :new-index next-index})
-    :else {:new-tape  tape
-           :new-index next-index}))
+(defn- next-rule [{:keys [rules state tape head]}]
+  (get (rules state) (tape head)))
 
 
-(defn- apply-rule [rule tape index pad-with]
-  (let [{:keys [new-tape new-index]}
-        (expand-tape
-          (+ index (:move rule))
-          (assoc tape index (:write rule))
-          pad-with)]
-    {:new-state (:state rule)
-     :new-index new-index
-     :new-tape  new-tape}))
+(defn- apply-rule [ar rule]
+  (let [head  (:head ar)
+        move  (:move rule)
+        write (:write rule)
+        tape  (:tape ar)
+        state (:state rule)]
+    (conj ar
+          {:head (+ head move)
+           :tape (assoc tape head write)
+           :state state})))
 
 
-(defn simulate
-  "Simulate a Turing Machine."
-  [tape state rules index pad-with]
-  (if-let [rule (lookup-rule rules state (tape index))]
-    (let [{:keys [new-state new-index new-tape]}
-          (apply-rule rule tape index pad-with)]
-      (recur new-tape new-state rules new-index pad-with))
-    {:tape tape, :index index, :state state}))
+(defn run [ar]
+  (let [<-ar-> (expand-tape ar)]
+    (if-let [rule (next-rule <-ar->)]
+      (recur (apply-rule <-ar-> rule))
+      ar)))
 
 
 (comment
 
-  (def binary-increment-rules
-    {:a {nil {:write nil
-              :state :b
-              :move  1}
-         0 {:write 0
-            :state :a
-            :move  -1}
-         1 {:write 1
-            :state :a
-            :move  -1}}
-     :b {nil {:write 1
-              :state :c
-              :move  -1}
-         0 {:write 1
-            :state :c
-            :move  1}
-         1 {:write 0
-            :state :b
-            :move  1}}
-     :c {nil {:write nil
-              :state :halt
-              :move  1}
-         0 {:write 0
-            :state :c
-            :move  -1}
-         1 {:write 1
-            :state :c
-            :move  -1}}
-     :halt nil})
+  (def erase-left-to-right-rules
+    {:a {nil {:write nil, :state :halt, :move 0}
+         0   {:write nil, :state :a,    :move 1}
+         1   {:write nil, :state :a,    :move 1}}})
 
-  (turing-machine/simulate [1 1 0 1] :a binary-increment-rules 0 nil)
+  (def binary-increment-rules
+    {:a {nil {:write nil, :state :b,    :move   1}
+         0   {:write 0,   :state :a,    :move  -1}
+         1   {:write 1,   :state :a,    :move  -1}}
+     :b {nil {:write 1,   :state :c,    :move  -1}
+         0   {:write 1,   :state :c,    :move   1}
+         1   {:write 0,   :state :b,    :move   1}}
+     :c {nil {:write nil, :state :halt, :move   1}
+         0   {:write 0,   :state :c,    :move  -1}
+         1   {:write 1,   :state :c,    :move  -1}}})
+
+  (:tape
+    (turing-machine/run
+      {:rules binary-increment-rules
+       :state :a
+       :tape [1 1 0 1]
+       :head 0
+       :blank nil}))
 
   )
