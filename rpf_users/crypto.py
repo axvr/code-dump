51046e0b8888431173f29f9d9b96bb4a88ec65cf
Alex Vear <alex@vear.uk>
2021-09-27 14:58:17 +0100

    Add function to coerce input to bytes in the crypto module

--- rpf_users/crypto.py
+++ rpf_users/crypto.py
@@ -2,14 +2,20 @@
 
 import nacl.pwhash
 
+def __coerce_to_bytes(txt):
+    if isinstance(txt, str):
+        return txt.encode()
+    elif isinstance(txt, bytes):
+        return txt
+    else:
+        raise TypeError('Value must be a string or byte array.')
+
 def hash_password(pwd):
-    if isinstance(pwd, str):
-        pwd = pwd.encode()
+    pwd = __coerce_to_bytes(pwd)
     return nacl.pwhash.argon2id.str(pwd)
 
 def verify_password(hash, pwd):
-    if isinstance(pwd, str):
-        pwd = pwd.encode()
+    pwd = __coerce_to_bytes(pwd)
     try:
         return nacl.pwhash.verify(hash, pwd)
     except nacl.exceptions.InvalidkeyError:
