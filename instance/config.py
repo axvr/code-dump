5425d245fb6466d6959bd913320c342cd9efa2c5
Alex Vear <alex@vear.uk>
2021-09-27 13:44:47 +0100

    Source configuration files
    Note that the file is included only because this is an example API.

---
+++ instance/config.py
@@ -0,0 +1,4 @@
+import os
+
+SECRET_KEY = 'very secret key'
+DATABASE = os.path.join(os.path.dirname(__file__), 'rpf_users.db')
--- rpf_users/__init__.py
+++ rpf_users/__init__.py
@@ -1,7 +1,20 @@
+import os
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
+def create_app(config=None):
+    """Create a new instance of the app."""
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_pyfile('config.py', silent=True)
+
+    if config:
+        app.config.from_mapping(config)
+
+    # Ensure instance directory exists to store SQLite DB in.
+    try:
+        os.makedirs(app.instance_path)
+    except FileExistsError: pass
 
     return app
 
